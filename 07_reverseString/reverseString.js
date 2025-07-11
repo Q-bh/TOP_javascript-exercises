79daa8559517e1fa3fad0f80ee4411a01626f419
Q-bh <kevinknguyen98@yahoo.com>
2025-07-11 01:37:41 -0700

    Complete exercise 7

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,9 +1,5 @@
 const reverseString = function(string) {
-    try {
-        return string[1:];
-    } catch (error) {
-        return "ERROR";
-    }
+    return string.split("").reverse().join("")
 };
 
 // Do not edit below this line
