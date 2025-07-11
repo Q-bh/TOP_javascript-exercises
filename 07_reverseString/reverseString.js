c9e79534e4b4f7c2c5c88c59c1f0e6fa579a8803
Q-bh <kevinknguyen98@yahoo.com>
2025-07-11 01:35:42 -0700

    Complete exercise 6

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    try {
+        return string[1:];
+    } catch (error) {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
